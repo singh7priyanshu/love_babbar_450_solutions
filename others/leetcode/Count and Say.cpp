f4256c600645420747dfc6bf31a87d1aea0896f5
priyanshu singh <singh7priyanshu@gmail.com>
2021-11-30 20:51:39 +0530

    Add files via upload

---
+++ others/leetcode/Count and Say.cpp
@@ -0,0 +1,62 @@
+/*
+Count and Say
+
+The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+
+countAndSay(1) = "1"
+countAndSay(n) is the way you would "say" the digit string from countAndSay(n-1), which is then converted into a different digit string.
+To determine how you "say" a digit string, split it into the minimal number of groups so that each group is a contiguous section all of the same character. Then for each group, say the number of characters, then say the character. To convert the saying into a digit string, replace the counts with a number and concatenate every saying.
+
+For example, the saying and conversion for digit string "3322251":
+
+
+Given a positive integer n, return the nth term of the count-and-say sequence.
+
+ 
+
+Example 1:
+
+Input: n = 1
+Output: "1"
+Explanation: This is the base case.
+Example 2:
+
+Input: n = 4
+Output: "1211"
+Explanation:
+countAndSay(1) = "1"
+countAndSay(2) = say "1" = one 1 = "11"
+countAndSay(3) = say "11" = two 1's = "21"
+countAndSay(4) = say "21" = one 2 + one 1 = "12" + "11" = "1211"
+ 
+
+Constraints:
+
+1 <= n <= 30
+
+*/
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        vector<string>v(31);
+        v[0]="0";
+        v[1]="1";
+        for(int i=2;i<=30;i++){
+            int x=i;
+            string temp = v[i-1],result = "";
+            int j=0;
+            while(j<temp.length()){
+                int count = 1;
+                while(j+1<temp.length() && temp[j] == temp[j+1]){
+                    count++;
+                    j++;
+                }
+                result+=to_string(count)+temp[j];
+                j++;
+            }
+            v[i] = result;
+        }
+        return v[n];
+    }
+};
