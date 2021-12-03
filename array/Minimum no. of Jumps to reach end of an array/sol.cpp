0b134c9a610034f8cebb7bcb4d439d0b509db330
priyanshu singh <singh7priyanshu@gmail.com>
2021-12-03 14:33:29 +0530

    Update sol.cpp

--- array/Minimum no. of Jumps to reach end of an array/sol.cpp
+++ array/Minimum no. of Jumps to reach end of an array/sol.cpp
@@ -1,6 +1,8 @@
 /*
 Minimum number of jumps
-Given an array of N integers arr[] where each element represents the max number of steps that can be made forward from that element. Find the minimum number of jumps to reach the end of the array (starting from the first element). If an element is 0, then you cannot move through that element.
+Given an array of N integers arr[] where each element represents the max number of steps that can be made forward from that element. 
+Find the minimum number of jumps to reach the end of the array (starting from the first element). If an element is 0, then you cannot 
+move through that element.
 Note: Return -1 if you can't reach the end of the array.
 
 
