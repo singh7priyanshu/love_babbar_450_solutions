876ccad621a155baa1319ba465e96e2eaa04ca2b
priyanshu singh <singh7priyanshu@gmail.com>
2021-12-23 19:04:20 +0530

    Add files via upload

---
+++ others/leetcode/69. Sqrt(x).cpp
@@ -0,0 +1,44 @@
+/*
+69. Sqrt(x)
+
+Given a non-negative integer x, compute and return the square root of x.
+
+Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+
+Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
+
+ 
+
+Example 1:
+
+Input: x = 4
+Output: 2
+Example 2:
+
+Input: x = 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since the decimal part is truncated, 2 is returned.
+ 
+
+Constraints:
+
+0 <= x <= 231 - 1
+
+*/
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long int i=1;
+        while(i){
+            if(i*i<x){
+                i++;
+                continue;
+            }else{
+                break;
+            }
+        }
+        if(i*i == x) return i;
+        else return i-1;
+    }
+};
