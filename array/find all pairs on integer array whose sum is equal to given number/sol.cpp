d2b417bbcb1d5c92355a925ea227837582a63617
priyanshu singh <singh7priyanshu@gmail.com>
2022-01-31 23:37:09 +0530

    Add files via upload

---
+++ array/find all pairs on integer array whose sum is equal to given number/sol.cpp
@@ -0,0 +1,71 @@
+/*
+Count pairs with given sum
+
+Given an array of N integers, and an integer K, find the number of pairs of elements in the array whose sum is equal to K.
+
+
+Example 1:
+
+Input:
+N = 4, K = 6
+arr[] = {1, 5, 7, 1}
+Output: 2
+Explanation: 
+arr[0] + arr[1] = 1 + 5 = 6 
+and arr[1] + arr[3] = 5 + 1 = 6.
+
+Example 2:
+
+Input:
+N = 4, X = 2
+arr[] = {1, 1, 1, 1}
+Output: 6
+Explanation: 
+Each 1 will produce sum 2 with any 1.
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function getPairsCount() which takes arr[], n and k as input parameters and returns the number of pairs that have sum K.
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(N)
+
+Constraints:
+1 <= N <= 105
+1 <= K <= 108
+1 <= Arr[i] <= 106
+
+*/
+
+#include<bitset>/stdc++.h>
+using namespace std;
+
+class Solution{   
+public:
+    int getPairsCount(int arr[], int n, int k) {
+        // code here
+        unordered_map<int, int>m;
+        for(int i = 0;i<n;i++)m[arr[i]]++;
+        int doublecount = 0;
+        for(int i = 0;i<n;i++){
+            doublecount += m[k-arr[i]];
+            if(k-arr[i] == arr[i]) doublecount--;
+        }
+        return doublecount/2;
+    }
+};
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        int arr[n];
+        for(int i = 0;i<n;i++)cin>>arr[i];
+        Solution ob;
+        auto ans = ob.getPairsCount(arr,n,k);
+        cout<<ans<<"\n";
+    }
+    return 0;
+}
