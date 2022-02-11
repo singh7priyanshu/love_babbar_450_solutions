9ecc8bff262323377b0bc57d72376ebe50e3d82c
priyanshu singh <singh7priyanshu@gmail.com>
2022-02-11 15:19:22 +0530

    Add files via upload

---
+++ strings/EDIT Distance [Very Imp]/sol.cpp
@@ -0,0 +1,81 @@
+/*
+EDIT Distance [Very Imp]
+
+Given two strings s and t. Return the minimum number of operations required to convert 
+s to t.
+The possible operations are permitted:
+
+Insert a character at any position of the string.
+Remove any character from the string.
+Replace any character from the string with any other character.
+ 
+
+Example 1:
+
+Input: 
+s = "geek", t = "gesek"
+Output: 1
+Explanation: One operation is required 
+inserting 's' between two 'e's of str1.
+Example 2:
+
+Input : 
+s = "gfg", t = "gfg"
+Output: 
+0
+Explanation: Both strings are same.
+ 
+
+Your Task:
+You don't need to read or print anything. Your task is to complete the function 
+editDistance() which takes strings s and t as input parameters and returns the minimum 
+number of operation to convert the string s to string t. 
+
+
+Expected Time Complexity: O(|s|*|t|)
+Expected Space Complexity: O(|s|*|t|)
+
+
+Constraints:
+1 ≤ Length of both strings ≤ 100
+Both the strings are in lowercase.
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int dp[102][102];
+    int fun(string s, string t, int pos1, int pos2){
+        if(pos1 == 0)return pos2;
+        if(pos2 == 0)return pos1;
+        int &ans = dp[pos1][pos2];
+        if(ans != -1)return ans;
+        if(s[pos1 - 1] == t[pos2-1])return ans = fun(s,t,pos1-1,pos2-1);
+        return ans = min({1+fun(s, t, pos1, pos2-1),
+                          1+fun(s, t, pos1-1, pos2),
+                          1+fun(s, t, pos1-1, pos2-1)
+        });
+    }
+    int editDistance(string s, string t){
+        memset(dp, -1, sizeof(dp));
+        int ans = fun(s, t, s.size(), t.size());
+        return ans;
+    }
+};
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s, t;
+        cin>>s>>t;
+        Solution ob;
+        int ans = ob.editDistance(s, t);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
+
