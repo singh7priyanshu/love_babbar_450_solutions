b5cb721b82418ebf71610cd4f58d7780795bd35d
priyanshu singh <singh7priyanshu@gmail.com>
2022-02-23 19:19:11 +0530

    Add files via upload

---
+++ others/leetcode/171. Excel Sheet Column Number.cpp
@@ -0,0 +1,54 @@
+/*
+171. Excel Sheet Column Number
+
+Given a string columnTitle that represents the column title as appear in an Excel sheet, 
+return its corresponding column number.
+
+For example:
+
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+ 
+
+Example 1:
+
+Input: columnTitle = "A"
+Output: 1
+Example 2:
+
+Input: columnTitle = "AB"
+Output: 28
+Example 3:
+
+Input: columnTitle = "ZY"
+Output: 701
+ 
+
+Constraints:
+
+1 <= columnTitle.length <= 7
+columnTitle consists only of uppercase English letters.
+columnTitle is in the range ["A", "FXSHRXW"].
+
+*/
+
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int result = 0;
+        for(char c : columnTitle){
+			//d = s[i](char) - 'A' + 1 (we used  s[i] -  'A' to convert the letter to a 
+            //number like it's going to be C)
+
+            int d = c - 'A' + 1 ;
+            result = result * 26 + d;
+        }
+        return result;
+    }
+};
