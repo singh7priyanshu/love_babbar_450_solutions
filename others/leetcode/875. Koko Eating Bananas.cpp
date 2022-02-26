9f4f465a309988d4195d62869b28e652e2025a09
priyanshu singh <singh7priyanshu@gmail.com>
2022-02-26 15:30:07 +0530

    Update 875. Koko Eating Bananas.cpp

--- others/leetcode/875. Koko Eating Bananas.cpp
+++ others/leetcode/875. Koko Eating Bananas.cpp
@@ -54,3 +54,22 @@ class Solution {
     } 
 };
 
+//OR
+
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int H) {
+        int low = 1, high = 1000000000, k = 0;
+        while (low <= high) {
+            k = (low + high) / 2;
+            int h = 0;
+            for (int i = 0; i < piles.size(); i ++) 
+                h += ceil(1.0 * piles[i] / k);
+            if (h > H)
+                low = k + 1;
+            else
+                high = k - 1;
+        }
+        return low;
+    }
+};
