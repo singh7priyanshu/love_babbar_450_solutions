9980017d1c07dfe48c4002034d03b07d233b87c4
priyanshu singh <singh7priyanshu@gmail.com>
2022-03-04 21:22:38 +0530

    Add files via upload

---
+++ searching&sorting/Findthe inversion count/sol.cpp
@@ -0,0 +1,121 @@
+/*
+Findthe inversion count
+
+Given an array of integers. Find the Inversion Count in the array. 
+
+Inversion Count: For an array, inversion count indicates how far (or close) 
+the array is from being sorted. If array is already sorted then the inversion 
+count is 0. If an array is sorted in the reverse order then the 
+inversion count is the maximum. 
+Formally, two elements a[i] and a[j] form an inversion if a[i] > a[j] and i < j.
+ 
+
+Example 1:
+
+Input: N = 5, arr[] = {2, 4, 1, 3, 5}
+Output: 3
+Explanation: The sequence 2, 4, 1, 3, 5 
+has three inversions (2, 1), (4, 1), (4, 3).
+
+Example 2:
+
+Input: N = 5
+arr[] = {2, 3, 4, 5, 6}
+Output: 0
+Explanation: As the sequence is already 
+sorted so there is no inversion count.
+
+Example 3:
+
+Input: N = 3, arr[] = {10, 10, 10}
+Output: 0
+Explanation: As all the elements of array 
+are same, so there is no inversion count.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the 
+function inversionCount() which takes the array arr[] and the size of the array 
+as inputs and returns the inversion count of the given array.
+
+Expected Time Complexity: O(NLogN).
+Expected Auxiliary Space: O(N).
+
+Constraints:
+1 ≤ N ≤ 5*105
+1 ≤ arr[i] ≤ 1018
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+  public:
+    
+    long long my_counter = 0;
+    
+    // arr[]: Input Array
+    // N : Size of the Array arr[]
+    // Function to count inversions in the array.
+    
+    void mergeSort(long long a[], long long p, long long r){
+        if(p<r){
+            long long q = (p+r)/2;
+            mergeSort(a, p, q);
+            mergeSort(a, q+1, r);
+            merge(a, p, q, r);
+        }
+    }
+    
+    void merge(long long a[], long long p, long long q, long long r){
+        long long l = q-p+1;
+        long long a1[l];
+        long long l2 = r-q;
+        long long a2[l2];
+        for(long long i = 0;i<l;i++){
+            a1[i] = a[i+p];
+        }
+        for(long long i = 0;i<l2;i++){
+            a2[i] = a[q+i+1];
+        }
+        long long left = 0, right = 0, k = p;
+        while(left<l && right < l2){
+            if(a1[left] <= a2[right]){
+                a[k] = a1[left];
+                left++;
+            }
+            else{
+                a[k] = a2[right];
+                right++;
+                my_counter += (l-left);
+            }
+            k++;
+        }
+        while(left<l){
+            a[k++] = a1[left++];
+        }
+        while(right < l2){
+            a[k++] = a2[right++];
+        }
+    }
+    
+    long long int inversionCount(long long arr[], long long N)
+    {
+        // Your Code Here
+        mergeSort(arr,0,N-1);
+        long long int res = my_counter;
+        my_counter = 0;
+        return res;
+    }
+
+};
+
+int main(){
+    long long t; cin>>t;
+    while(t--){
+        long long n; cin>>n;
+        long long a[n];
+        for(long long i = 0;i<n;i++)cin>>a[i];
+        Solution ob;
+        cout<<ob.inversionCount(a, n)<<endl;
+    }
+    return 0;
+}
